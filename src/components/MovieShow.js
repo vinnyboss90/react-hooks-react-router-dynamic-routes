d4766873c4dabb3814b368fa467b7c504aba2893
vinnyegon90 <vinnyegon90@gmail.com>
2024-02-10 19:30:09 +0300

    Done

---
+++ src/components/MovieShow.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function MovieShow({ movies }) {
+  // call useParams to access the `params` from the url
+  const params = useParams();
+  console.log(params);
+
+  const movie = movies.find(movie => movie.id === parseInt(params.movieId))
+
+  return (
+    <div>
+      {/* And here we access the `movieId` stored in `params` to render 
+        information about the selected movie */}
+      <h3>{movie.title}</h3>
+    </div>
+  );
+}
+
+export default MovieShow;
